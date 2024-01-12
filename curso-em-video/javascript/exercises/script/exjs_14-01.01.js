dc0fa2c3662aa0cf0f187d8b06dfee3f5b38874b
Lero Perih <1joao2victor3santana4ferreira@gmail.com>
2024-01-12 10:42:26 -0300

    real

--- curso-em-video/javascript/exercises/script/exjs_14-01.01.js
+++ curso-em-video/javascript/exercises/script/exjs_14-01.01.js
@@ -5,58 +5,58 @@ h = agora.getHours()
 m = agora.getMinutes()
 
 
-hra = document.querySelector("div#hra")
 body = document.querySelector("body")
 box = document.querySelector("div#box")
 
 img = document.createElement("img")
 
 
-if ( h==0 ) { // meia noite
+  if      ( h==0 ) { // meia noite
     body.classList.add('meia-noite')
-    res.innerHTML = `Agora&nbsp;são&nbsp;${h}&nbsp;horas e&nbsp;${m}&nbsp;minutos`
+    box.innerHTML = `<p>Agora são ${h} horas e ${m} minutos</p>`
     box.appendChild(img)
     img.setAttribute("src", "conteudo/1-img_hora.png")
 
 
-} else if ( h<5 && m<45 ) { // madrugada
+} else if ( h>0 && h<6 ) { // madrugada
     body.classList.add('madrugada')
-    res.innerHTML = `Agora&nbsp;são&nbsp;${h}&nbsp;horas e&nbsp;${m}&nbsp;minutos`
+    box.innerHTML = `<p>Agora são ${h} horas e ${m} minutos</p>`
     box.appendChild(img)
     img.setAttribute("src", "conteudo/2-img_hora.png")
+    box.innerHTML += `<p>/* Melhor Horário */</p>`
 
 
-} else if (h<=6&&m<=15) { // amanhecer
+} else if ( h==6 ) { // amanhecer
     body.classList.add('amanhecer')
-    res.innerHTML = `Agora&nbsp;são&nbsp;${h}&nbsp;horas e&nbsp;${m}&nbsp;minutos`
+    box.innerHTML = `<p>Agora são ${h} horas e ${m} minutos</p>`
     box.appendChild(img)
     img.setAttribute("src", "conteudo/3-img_hora.png")
 
 
-} else if (h<12&&m<45) { // manhã
+} else if ( h>6 && h<12 ) { // manhã
     body.classList.add('manha')
-    res.innerHTML = `Agora&nbsp;são&nbsp;${h}&nbsp;horas e&nbsp;${m}&nbsp;minutos`
+    box.innerHTML = `<p>Agora são ${h} horas e ${m} minutos</p>`
     box.appendChild(img)
     img.setAttribute("src", "conteudo/4-img_hora.png")
 
 
-} else if (h<=13&&m<=15) { // meio dia
+} else if ( h==12 ) { // meio dia
     body.classList.add('meio-dia')
-    res.innerHTML = `Agora&nbsp;são&nbsp;${h}&nbsp;horas e&nbsp;${m}&nbsp;minutos`
+    box.innerHTML = `<p>Agora são ${h} horas e ${m} minutos</p>`
     box.appendChild(img)
     img.setAttribute("src", "conteudo/5-img_hora.png")
 
 
-} else if (h<17&&m<45) { // tarde
+} else if ( h>12 && h<18 ) { // tarde
     body.classList.add('tarde')
-    res.innerHTML = `Agora&nbsp;são&nbsp;${h}&nbsp;horas e&nbsp;${m}&nbsp;minutos`
+    box.innerHTML = `<p>Agora são ${h} horas e ${m} minutos</p>`
     box.appendChild(img)
     img.setAttribute("src", "conteudo/6-img_hora.png")
 
 
 } else if ( h==18 ) { // anoitecer
     body.classList.add('anoitecer')
-    res.innerHTML = `Agora&nbsp;são&nbsp;${h}&nbsp;horas e&nbsp;${m}&nbsp;minutos`
+    box.innerHTML = `<p>Agora são ${h} horas e ${m} minutos</p>`
     box.appendChild(img)
     img.setAttribute("src", "conteudo/7-img_hora.png")
 
